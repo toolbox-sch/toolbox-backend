8bbdb3f8e9ae46e572c126c407a926f9002f4fc3
Jiyong <hickerd@sch.ac.kr>
2024-06-02 17:09:48 +0900

    [Fix] 인증 완료 시 Response 변경

--- core/fastapi/dependencies/permission.py
+++ core/fastapi/dependencies/permission.py
@@ -1,15 +1,19 @@
 from abc import ABC, abstractmethod
 from typing import Type
 
+import jwt
 from dependency_injector.wiring import Provide, inject
 from fastapi import Depends, Request
-from fastapi.openapi.models import APIKey, APIKeyIn
+from fastapi.openapi.models import APIKey, APIKeyIn, HTTPBearer
+from fastapi.security import HTTPAuthorizationCredentials
 from fastapi.security.base import SecurityBase
+from fastapi.security.utils import get_authorization_scheme_param
 from starlette import status
 
 from app.container import Container
 from app.user.domain.usecase.user import UserUseCase
 from core.exceptions import CustomException
+from core.helpers.token import TokenHelper
 
 
 class UnauthorizedException(CustomException):
@@ -18,12 +22,18 @@ class UnauthorizedException(CustomException):
     message = ""
 
 
+class InvalidTokenException(CustomException):
+    code = status.HTTP_401_UNAUTHORIZED
+    error_code = "INVALID_TOKEN"
+    message = "Invalid token"
+
+
 class BasePermission(ABC):
     exception = CustomException
 
     @abstractmethod
     async def has_permission(self, request: Request) -> bool:
-        """has permssion"""
+        """has permission"""
 
 
 class IsAuthenticated(BasePermission):
@@ -54,14 +64,27 @@ class AllowAll(BasePermission):
         return True
 
 
-class PermissionDependency(SecurityBase):
+class PermissionDependency(HTTPBearer):
     def __init__(self, permissions: list[Type[BasePermission]]):
+        super().__init__(auto_error=False)
         self.permissions = permissions
-        self.model: APIKey = APIKey(**{"in": APIKeyIn.header}, name="Authorization")
-        self.scheme_name = self.__class__.__name__
 
     async def __call__(self, request: Request):
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            raise UnauthorizedException
+
+        try:
+            payload = TokenHelper.decode(param)
+            request.user.id = payload.get("user_id")
+        except jwt.PyJWTError:
+            raise InvalidTokenException
+
         for permission in self.permissions:
             cls = permission()
             if not await cls.has_permission(request=request):
                 raise cls.exception
+
+    def __hash__(self):
+        return hash(tuple(self.permissions))
